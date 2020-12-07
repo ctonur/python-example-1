afe2e8d3b36d4f57b7da970538f69c93060e27ba
Can Okcelik <cokcelik@tav.aero>
2020-12-07 17:35:08 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import time,os
+from flask import Flask
+app = Flask(__name__)
+
+hostname = os.environ['HOSTNAME']
+
+@app.route('/')
+def hello_world():
+    return hostname+'v4 - 15sn gecikmeli kalkis - Hey gidi koca dunya \n'
+
+if __name__ == '__main__':
+    time.sleep(15) # baslarken 15 saniye gecik, sonradan ayaga kalk
+    app.run(host='0.0.0.0',port=8080)
---
+++ curl-loop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while :
+do
+        curl python-test.okd.local
+        sleep 0.5
+done
