11b781fd4d4ffe5b9158afedf9e59cb37c422b17
ctonur <35571438+ctonur@users.noreply.github.com>
2025-05-16 11:16:53 +0200

    Update app.py

--- python-zero-downtime/app.py
+++ python-zero-downtime/app.py
@@ -6,8 +6,8 @@ hostname = os.environ.get('HOSTNAME','localhost')
 
 @app.route('/')
 def hello_world():
-    return hostname+'v4 - 15sn gecikmeli kalkis - Hey gidi koca dunya \n'
+    return hostname+'v4 - 25sn gecikmeli kalkis - Hey gidi koca dunya \n'
 
 if __name__ == '__main__':
-    time.sleep(15) # baslarken 15 saniye gecik, sonradan ayaga kalk
+    time.sleep(25) # baslarken 25 saniye gecik, sonradan ayaga kalk
     app.run(host='0.0.0.0',port=8080)
